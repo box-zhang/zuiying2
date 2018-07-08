a0b3c4ed41b8d859ad222bb63d15b37c7ad5f870
box-zhang <2680847417@qq.com>
2018-07-08 15:45:59 +0800

    登录注册

--- zuiYingDemo/zuiying_demo/js/common.js
+++ zuiYingDemo/zuiying_demo/js/common.js
@@ -23,6 +23,66 @@ $(document).ready(function () {
         $("#navbar").collapse('hide');
     });
 
+    // selected
+    var selectBox = $('.ivu-select');
+    selectBox.bind('click', function (e) {
+        var _this=$(this);
+        var selectDown = _this.find('.ivu-select-dropdown');
+        if (selectDown.css("display") == "none") {
+            selectDown.slideDown("fast");
+            _this.addClass('ivu-select-visible');
+        } else {
+            selectDown.slideUp("fast");
+            _this.removeClass('ivu-select-visible');
+        }
+        selectDown.find('li').click(function () {
+            var txt = $(this).text();
+            console.log(txt);
+            selectBox.find('span').html(txt).addClass('ivu-select-selected-value').removeClass('ivu-select-placeholder');
+            selectBox.find('input').val(txt);
+            selectDown.slideUp("fast");
+        });
+
+    });
+    //checkbox
+    var checkboxBox = $('.ivu-checkbox-wrapper');
+    checkboxBox.bind('click', function (e) {
+        var _this = $(this);
+        (function () {
+            if (_this.hasClass('ivu-checkbox-wrapper-checked')) {
+                _this.removeClass('ivu-checkbox-wrapper-checked');
+                _this.find('.ivu-checkbox').removeClass('ivu-checkbox-checked');
+                _this.find('input').removeAttr('checked', 'checked');
+            } else {
+                _this.addClass('ivu-checkbox-wrapper-checked');
+                _this.find('.ivu-checkbox').addClass('ivu-checkbox-checked');
+                _this.parent().parent().removeClass('ivu-form-item-error');
+                _this.find('.ivu-form-item-error-tip').remove();
+                _this.find('input').attr('checked', 'true');
+            }
+        })()
+
+    });
+    //input
+    (function () {
+        $(':input').on('keyup', function (e) {
+            var _this = $(this);
+            _this.val().replace('\s+', '');
+            if (_this.val() && _this.parents('.ivu-form-item').hasClass('ivu-form-item-error')) {
+                _this.parents('.ivu-form-item').removeClass('ivu-form-item-error')
+                _this.parents('.ivu-form-item-content').find('.ivu-form-item-error-tip').remove();
+
+            }
+        });
+        $(":input").blur(function(){
+            if (_this.val()==''&& !_this.parents('.ivu-form-item').hasClass('ivu-form-item-error')) {
+                _this.parents('.ivu-form-item').addClass('ivu-form-item-error');
+                _this.parents('.ivu-form-item-content').append('<div class="ivu-form-item-error-tip">'+'请输入...'+'</div>');
+            }
+            if(_this.find('.ivu-form-item-error-tip')){
+            }
+        });
+    })();
 
     var cbpAnimatedHeader = (function () {
         var docElem = document.documentElement,
@@ -65,17 +125,17 @@ $(document).ready(function () {
     var navAvatar = nav.find('.ivu-dropdown');
     var footer = $("#footer");
     var footerCode = footer.find('.ivu-tooltip');
-    navAvatar.hover(function() {
+    navAvatar.hover(function () {
         //你要显示的层，div放到li里面。默认css属性是隐藏
         $(this).find('.ivu-select-dropdown').stop().slideDown();
-    }, function() {
+    }, function () {
         //你要显示的列表形式
         $(this).find('.ivu-select-dropdown').stop().slideUp();
     });
-    footerCode.hover(function() {
+    footerCode.hover(function () {
         //你要显示的层，div放到li里面。默认css属性是隐藏
         $(this).find('.ivu-tooltip-popper').stop().fadeIn(200);
-    }, function() {
+    }, function () {
         //你要显示的列表形式
         $(this).find('.ivu-tooltip-popper').stop().fadeOut(200);
     });
@@ -86,7 +146,7 @@ $(document).ready(function () {
     //获取页面可视区域的高度
     var clientHeight = $(window).height();
     //鼠标向下滑动，出现以及消失
-    $(window).scroll(function() {
+    $(window).scroll(function () {
         var osTop = $(document).scrollTop();
         if (osTop >= 800) {
             goTop.addClass('ivu-back-top-show');
@@ -94,9 +154,9 @@ $(document).ready(function () {
             goTop.removeClass('ivu-back-top-show');
         }
     });
-    goTop.click(function() {
+    goTop.click(function () {
         //设置定时器
-        timer = setInterval(function() {
+        timer = setInterval(function () {
             //获取滚动条高度
             var osTop = $(document).scrollTop();
             //ispeed 最后等于0
