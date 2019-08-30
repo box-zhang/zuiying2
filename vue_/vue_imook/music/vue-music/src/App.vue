0c156d18cd231ae488c89616cb5f54b3447f76d7
box-zhang <2680847417@qq.com>
2019-08-30 17:36:05 +0800

    add all

---
+++ vue_/vue_imook/music/vue-music/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    Hello World!
+    <router-view />
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+@import 'common/stylus/variable';
+
+#app {
+  color: $color-theme;
+}
+</style>
